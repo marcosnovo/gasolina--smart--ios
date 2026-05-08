63531b54bb0e2a18b567c449f0f32549fd127b89
Marcos <marcosnovo@gmail.com>
2026-05-08 09:54:46 +0200

    Initial Commit

---
+++ GasolinaSmart/GasolinaSmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  GasolinaSmartApp.swift
+//  GasolinaSmart
+//
+//  Created by Marcos on 08/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct GasolinaSmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GasolinaSmartTests/GasolinaSmartTests.swift
@@ -0,0 +1,19 @@
+//
+//  GasolinaSmartTests.swift
+//  GasolinaSmartTests
+//
+//  Created by Marcos on 08/05/2026.
+//
+
+import Testing
+@testable import GasolinaSmart
+
+struct GasolinaSmartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
